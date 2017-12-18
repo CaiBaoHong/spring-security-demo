eec43edc662bb3ab86f98c3d22c2e448e948436f
蔡保洪@Home <caibaohong@juleisoft.com>
2017-12-19 01:51:09 +0800

    init the whole framework

---
+++ demo/src/main/java/com/abc/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.abc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by CaiBaoHong at 2017/12/18
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class,args);
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello spring security demo";
+    }
+
+}
